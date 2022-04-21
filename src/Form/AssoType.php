c458123a4b651dc8ebf50821d72dc2c6211e40fc
huiitre <yanis.deluca@laposte.net>
2022-04-21 16:50:36 +0200

    add choiceType on update - facto api dept and region

--- src/Form/AssoType.php
+++ src/Form/AssoType.php
@@ -13,32 +13,44 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Positive;
+use App\Service\GeolocApi;
 
 class AssoType extends AbstractType
 {
+    private $department;
+
+    public function __construct(GeolocApi $department)
+    {
+        $this->department = $department;
+    }
+
     /**
      * Propriétés et vérifications nécessaires au formulaire
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $department = $this->department->fetch("departements");
+        $region = $this->department->fetch("regions");
         $builder
             /* ->add('type') */
             ->add('name', TextType::class, ['label' => 'Nom de l\'association'])
             ->add('siret', TextType::class, [
                 'constraints' => [new Length(['min' => 17, 'max' => 17])]
-                ])
+            ])
             ->add('mail', EmailType::class, ['label' => 'E-Mail'])
             // ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
             ->add('adress', TextType::class, ['label' => 'Adresse'])
             ->add('zipcode', TextType::class, ['label' => 'Code postal'])
             ->add('city', TextType::class, ['label' => 'Ville'])
-            ->add('department', TextType::class, ['label' => 'Département'])
-            ->add('region', TextType::class, ['label' => 'Région'])
+            // ->add('department', TextType::class, ['label' => 'Département'])
+            ->add('department', ChoiceType::class, ['label' => 'Département', 'choices' => $department])
+            ->add('region', ChoiceType::class, ['label' => 'Région', 'choices' => $region])
             ->add('phone_number', TextType::class, ['label' => 'Numero de téléphone'])
             ->add('description', TextareaType::class, ['label' => 'Description'])
             ->add('picture', TextareaType::class, ['label' => 'Photo'])
@@ -47,10 +59,9 @@ class AssoType extends AbstractType
             ->add('species', EntityType::class, [
                 'class' => Species::class,
                 'choice_label' => 'name',
-                'multiple' => true, 
+                'multiple' => true,
                 'expanded' => true,
-            ])
-            ;
+            ]);
     }
 
     /**
